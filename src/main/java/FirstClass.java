e9f90a2e205f2d661f6b959acbce1cbaf4d59962
Mateusz Filip <starszy1994@gmail.com>
2021-12-27 16:57:56 +0100

    Initial version

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class FirstClass {
+public class FirstClass {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello from FirstClass!"); // [3]
+    }
 }
