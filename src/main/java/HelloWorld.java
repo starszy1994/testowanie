cea57a230c36ac0f487a219ceeef551a96084297
Mateusz Filip <starszy1994@gmail.com>
2021-12-27 21:54:40 +0100

    Initial version

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Mateusz");
+        System.out.println(27);
+        System.out.println('M');
+    }
 }
---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,10 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myClub = "Real Madrid";
+        int favouritNumber = 6;
+        double weigh = 77.50;
+        char energyClass = 'B';
+        boolean imMan = true;
+        System.out.println(energyClass);
+    }
+}
